c193d33e2c431034ea970c97346dffba2ca0d1b1
It's Ami <bia982497@gmail.com>
2025-08-31 16:05:10 +0000

    # To learn more about how to use Nix to configure your environment # see

---
+++ .nix/config.nix
@@ -0,0 +1,48 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{pkgs}: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.zulu
+    # Adding Python and the required library for the Telegram bot
+    pkgs.python311Full
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.python-telegram-bot
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
+  # a firebase.json file is written into the user's directory
+  services.firebase.emulators = {
+    # Disabling because we are using prod backends right now
+    detect = false;
+    projectId = "demo-app";
+    services = ["auth" "firestore"];
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      onCreate = {
+        default.openFiles = [
+          "src/app/page.tsx"
+        ];
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
