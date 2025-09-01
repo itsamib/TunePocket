e01feecea4e347df6cedc36fcf899ad46ffad369
It's Ami <bia982497@gmail.com>
2025-09-01 01:56:10 +0000

    بازم همونشد که

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -18,8 +18,8 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-transform"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className="h-full w-full flex-1 bg-primary transition-all"
+      style={{ width: `${value || 0}%` }}
     />
   </ProgressPrimitive.Root>
 ))
